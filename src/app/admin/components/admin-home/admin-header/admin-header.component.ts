0a9156ca617c2a58fd08b17c881f88a0d3bb9ff1
maiquocbao666 <maiquocbao666@gmail.com>
2023-11-06 12:54:01 +0700

    Loại bỏ css header admin

--- src/app/admin/components/admin-home/admin-header/admin-header.component.ts
+++ src/app/admin/components/admin-home/admin-header/admin-header.component.ts
@@ -6,27 +6,11 @@ declare var $: any;
   styleUrls: ['./admin-header.component.css']
 })
 export class AdminHeaderComponent {
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+  
+  constructor() {
   }
   
   ngOnInit(): void {
-    this.sideNav(); // Kiểm tra trạng thái ban đầu
-
-    // Kiểm tra kích thước cửa sổ khi cửa sổ thay đổi
-    this.renderer.listen('window', 'resize', () => {
-      this.sideNav();
-    });
-  }
-  sideNav() {
-    if (window.innerWidth <= 1133) {
-      this.renderer.setStyle(this.elementRef.nativeElement.querySelector('.sidebar'), 'display', 'none');
-    } else {
-      this.renderer.removeStyle(this.elementRef.nativeElement.querySelector('.sidebar'), 'display');
-    }
   }
   
-  toggleCanvas() {
-    // Xử lý sự kiện click cho .canvas-toggle
-    // Đặt logic xử lý tại đây
-  }
 }
