c1c0360142643ca904772d2c0e042a7409baaf1d
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-11-23 00:02:59 +0700

    tạo box chat message(đang thử nghiệm)

--- src/app/user/components/user-home/user-home.component.ts
+++ src/app/user/components/user-home/user-home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare global {
+  interface Window { fbAsyncInit: () => void; }
+}
+
+declare const FB: any;
 
 @Component({
   selector: 'app-user-home',
   templateUrl: './user-home.component.html',
   styleUrls: ['./user-home.component.css']
 })
-export class UserHomeComponent {
+export class UserHomeComponent implements OnInit {
 
+  ngOnInit() {
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: 'v18.0'
+      });
+    };
+
+    (function (d, s, id) {
+      var js: HTMLScriptElement, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = <HTMLScriptElement>document.createElement(s); // Ép kiểu thành HTMLScriptElement
+      js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      if (fjs && fjs.parentNode) {
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }(document, 'script', 'facebook-jssdk'));
+
+    var chatbox = document.getElementById('fb-customer-chat');
+    if (chatbox) {
+      chatbox.setAttribute("page_id", "162516943617657");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    }
+  }
 }
