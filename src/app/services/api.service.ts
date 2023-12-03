9eb3d1143c3770b982042a36322fe3dec6bb200b
maiquocbao666 <maiquocbao666@gmail.com>
2023-12-03 12:04:54 +0700

    Deploy

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -28,9 +28,9 @@ export class ApiService {
 
   setUrl(uri: string) {
 
-    this.baseUrl = 'http://localhost:8080/public/' + uri;
-    console.log('API Endpoint:', this.baseUrl);
-    //this.baseUrl = 'https://springleafrestaurantbackend.onrender.com/public/' + uri;
+    //this.baseUrl = 'http://localhost:8080/public/' + uri;
+    //console.log('API Endpoint:', this.baseUrl);
+    this.baseUrl = 'https://springleafrestaurantbackend.onrender.com/public/' + uri;
 
     //this.baseUrl = 'http://localhost:8080/public/' + uri;
   }
